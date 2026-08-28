f894f78492cef828023ef4d13847795008cb780f
CodingCitron <78482307+CodingCitron@users.noreply.github.com>
2026-08-28 15:08:28 +0900

    wip: 참조표목조회 버튼 작업중...

--- src/components/authority-corporation-form-page/authority-corporation-form.tsx
+++ src/components/authority-corporation-form-page/authority-corporation-form.tsx
@@ -14,6 +14,7 @@ import {
   type CorporationAuthorityFormValues,
   type CorporationMarcAddTarget,
 } from "./corporation-form.mapper";
+import { AuthorityReferenceHeadingSearchButton } from "../ui/authority-reference-heading-search-modal";
 
 type TextFieldName = FieldPathByValue<CorporationAuthorityFormValues, string>;
 
@@ -194,14 +195,7 @@ export default function AuthorityCorporationForm({
                   </span>
                 </div>
                 <div className="col">
-                  <button
-                    type="button"
-                    className="btn btn-secondary w-100"
-                    data-bs-toggle="modal"
-                    data-bs-target="#modal5XX"
-                  >
-                    참조표목조회(5XX) 추가
-                  </button>
+                  <AuthorityReferenceHeadingSearchButton />
                 </div>
               </div>
               <div className="row g-2 align-items-start">
---
+++ src/components/ui/authority-reference-heading-search-modal.tsx
@@ -0,0 +1,22 @@
+export function AuthorityReferenceHeadingSearchButton() {
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary w-100"
+        data-bs-toggle="modal"
+        data-bs-target="#modal5XX"
+      >
+        참조표목조회(5XX) 추가
+      </button>
+    </>
+  );
+}
+
+export function AuthorityReferenceHeadingSearchModal() {
+  return;
+}
+
+export function AuthorityReferenceHeadingSearchModalBody() {
+  return;
+}
